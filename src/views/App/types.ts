1eae05219cf24d9045df9a6a372d41b9810dba46
fancyzhong <fancyzhong@tencent.com>
2020-11-20 13:34:41 +0800

    add SettingsType

--- src/views/App/types.ts
+++ src/views/App/types.ts
@@ -2,6 +2,7 @@ import { AnyObject, CtxM } from '@/models/types'
 import { SettingsType } from 'concent';
 import { setup } from './setup';
 
+// 使用SettingsType求出settings类型定义，传递给CtxM，方便用户可以在组件里使用 settins.{name}时 能够跳转到源码实现处
 type Se = SettingsType<typeof setup>;
 
 // setup 参数里用的Ctx 还没有settings
